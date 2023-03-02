2f5b4a453a45bd92b7006c6f40b87c2f1b38f83a
Dustin Dixon <bobdeus@gmail.com>
2023-03-02 16:14:09 -0700

    initial commit

---
+++ src/main/java/com/bracbrun/multipass/MultiPassApplication.java
@@ -0,0 +1,13 @@
+package com.bracbrun.multipass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiPassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiPassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bracbrun/multipass/MultiPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bracbrun.multipass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiPassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
