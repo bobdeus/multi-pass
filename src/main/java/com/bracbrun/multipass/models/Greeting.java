280a85952a874a057847b680fb9e939485737652
Dustin Dixon <bobdeus@gmail.com>
2023-03-06 09:09:25 -0700

    minor reformat

--- src/main/java/com/bracbrun/multipass/models/Greeting.java
+++ src/main/java/com/bracbrun/multipass/models/Greeting.java
@@ -3,4 +3,5 @@ package com.bracbrun.multipass.models;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Greeting(long id,  String content) { }
+public record Greeting(long id, String content) {
+}
